f1982ca98774825e0702c203b386fce935e34757
Derek Winters <github@derekwinters.com>
2021-08-17 13:53:58 -0500

    Create variables.tf

---
+++ centralus/variables.tf
@@ -0,0 +1,14 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "product_name" {
+  description = "Product name"
+  type        = string
+}

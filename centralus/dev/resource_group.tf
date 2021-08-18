bd4e20b657df8d7b22f4d64212edfce43d5d55dd
Derek Winters <github@derekwinters.com>
2021-08-17 22:17:10 -0500

    Update resource_group.tf

--- centralus/dev/resource_group.tf
+++ centralus/dev/resource_group.tf
@@ -2,12 +2,13 @@ module "resource_group" {
   source  = "my-tf-registry.example.net/myorg/terraform-azurerm-resource-group-module"
   version = "< 2"
 
-  product_name = var.product_name
-  location     = var.location
-  environment  = var.environment
-  repository   = var.repository
+  # This module accepts a defined object as the required_tags variable which are
+  # used to generate the resource group name
+  required_tags = local.required_tags
 
+  # The additional_tags variable is an arbitrary map. When merged with required_tags
+  # to create the resource group. Keys in required_tags take precedence.
   additional_tags = {
-    my_tag = "Example resource group to show variables"
+    my_tag = "Example resource group to show tag variables"
   }
 }
